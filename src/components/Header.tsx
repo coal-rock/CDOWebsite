9e6f3307b3f38509edb40d9b56ddf35bc1213e94
coal-rock <colebrazinski15@gmail.com>
2025-10-04 01:50:20 -0400

    feat: cleanup navbar + add style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,27 +4,33 @@ import logo from '../assets/logo.png'
 function Header() {
     return (
         <>
-            <nav className="bg-gray-950">
+            <nav className="bg-gray-950 w-full h-auto border-b-indigo-600 border-solid border-1">
                 <div className="mx-auto max-w-9xl px-2 sm:px-6 lg:px-8">
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="flex flex-row justify-center items-center">
                             <img src={logo} className="h-12 pr-2">
                             </img>
-                            <Link to="/" className="text-2xl text-white hover:underline">
+                            <Link to="/" className="text-2xl text-white font-semibold font-mono hover:underline">
                                 {"Cyber Defense Organization"}
                             </Link>
                         </div>
-                        <div className="flex">
+                        <div className="flex flex-row">
                             {[
-                                ["/Teams", "/teams"],
-                                ["/GDDC", "/gddc"],
-                                ["/Content", "/content"],
-                                ["/Resources", "/resources"],
-                                ["/About", "/about"],
-                                ["/Alumni", "/alumni"],
+                                ["Home", "/"],
+                                ["Teams", "/teams"],
+                                ["GDDC", "/gddc"],
+                                ["Content", "/content"],
+                                ["Resources", "/resources"],
+                                ["About", "/about"],
+                                ["Alumni", "/alumni"],
                             ].map((route) => (
-                                <Link className="text-xl text-white p-2 hover:underline" to={route[1]}>
-                                    {route[0]}
+                                <Link className="flex flex-row text-xl text-white font-mono p-2 hover:underline" to={route[1]}>
+                                    <p className="text-indigo-600">
+                                        {"/ "}
+                                    </p>
+                                    <p>
+                                        {route[0]}
+                                    </p>
                                 </Link>
                             ))}
                         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,19 @@ import Header from "../components/Header";
 function Home() {
     return (
         <>
-            <Header />
+            <div className="flex-col w-full h-full">
+                <Header />
+                <div className="flex flex-col items-center justify-between w-full h-full bg-gray-950">
+                    <div className="bg-white w-10 h-10">
+                    </div>
+
+                    <div className="bg-white w-10 h-10">
+                    </div>
+
+                    <div className="bg-white w-10 h-10">
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
