14de0c1e6bc503935aeb80146cf447f408d3296b
coal-rock <colebrazinski15@gmail.com>
2025-10-04 17:25:27 -0400

    feat: a lot of shit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,80 @@
 import { Link } from "react-router";
 import logo from '../assets/logo.png'
 
+import { useState } from "react";
+import { useLocation } from "react-router";
+
 function Header() {
+    const location = useLocation();
+    const [open, setOpen] = useState(false);
+
+    const routes = [
+        ["Home", "/"],
+        ["Teams", "/teams"],
+        ["GDDC", "/gddc"],
+        ["Content", "/content"],
+        ["Resources", "/resources"],
+        ["About", "/about"],
+        ["Alumni", "/alumni"]
+    ];
+
     return (
         <>
-            <header className="bg-gray-950 w-full h-auto border-b-indigo-600 border-solid border-1">
+            <header className="bg-gray-950 w-full h-auto border-b-indigo-400 border-solid border-1">
                 <div className="mx-auto max-w-9xl px-2 sm:px-4 lg:px-6">
-                    <div className="relative flex h-16 items-center justify-between">
+                    <div className="relative flex h-12 md:h-16 items-center justify-between">
                         <div className="flex flex-row justify-center items-center">
-                            <img src={logo} className="h-12 pr-2 pb-1.5">
+                            <img src={logo} className="h-10 pr-2 pb-0.5">
                             </img>
-                            <Link to="/" className="text-2xl text-white font-semibold font-mono hover:text-indigo-400 transition-colors duration-200">
+                            <Link to="/" className="hidden sm:block text-xl text-white font-semibold font-mono hover:text-indigo-400 transition-colors duration-200">
                                 {"Cyber Defense Organization"}
                             </Link>
+                            <Link to="/" className="sm:hidden text-xl text-white font-semibold font-mono">
+                                {"CDO"}
+                            </Link>
                         </div>
-                        <div className="flex flex-row">
-                            {[
-                                ["Home", "/"],
-                                ["Teams", "/teams"],
-                                ["GDDC", "/gddc"],
-                                ["Content", "/content"],
-                                ["Resources", "/resources"],
-                                ["About", "/about"],
-                                ["Alumni", "/alumni"],
-                            ].map((route) => (
-                                <Link className="flex flex-row text-xl text-gray-300 font-mono p-2 hover:text-white transition-colors duration-200" to={route[1]}>
-                                    <p className="text-indigo-600">
+                        <div className="flex-row hidden lg:flex">
+                            {routes.map((route) => (
+                                <Link className="flex flex-row text-xl text-gray-300 font-mono p-2 hover:text-white transition-colors duration-200" to={route[1]} >
+                                    <p className="text-indigo-400">
                                         {"/ "}
                                     </p>
-                                    <p>
+                                    <p className={(location.pathname == route[1]) ? "text-indigo-400" : ""}>
                                         {route[0]}
                                     </p>
                                 </Link>
                             ))}
+                        </div>
+
+                        <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" className="stroke-white lg:hidden" onClick={() => setOpen(true)}>
+                            <path d="M7 10H41" stroke-width="2" />
+                            <path d="M7 24H41" stroke-width="2" />
+                            <path d="M7 38H41" stroke-width="2" />
+                        </svg>
+
+                        <div
+                            className={`fixed inset-0 bg-black/40 backdrop-blur-sm transition-opacity duration-300 ${open ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                                }`}
+                            onClick={() => setOpen(false)}
+                        />
+
+                        <div
+                            className={`p-2 fixed top-0 right-0 h-full w-38 bg-gray-950 shadow-lg transform transition-transform duration-300 ${open ? "translate-x-0" : "translate-x-full"
+                                }`}
+
+                        >
+                            <div className="flex-row lg:flex">
+                                {routes.map((route) => (
+                                    <Link className="flex flex-row text-gray-300 font-mono p-2 hover:text-white transition-colors duration-200" to={route[1]} >
+                                        <p className="text-indigo-400">
+                                            {"/ "}
+                                        </p>
+                                        <p className={(location.pathname == route[1]) ? "text-indigo-400" : ""}>
+                                            {route[0]}
+                                        </p>
+                                    </Link>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,39 +2,39 @@ import Card from "../components/Card";
 import Header from "../components/Header";
 
 import hero from "../assets/hero.png";
+import workshop from "../assets/workshop.png"
+import community from "../assets/community.png"
+import competition from "../assets/competition.png"
+
 
 function Home() {
     return (
         <>
             <Header />
-            <div className="flex flex-col w-full h-full items-center bg-gray-950">
-                <div className="flex flex-col items-center justify-between w-5xl h-full pt-10">
+            <div className="flex flex-col w-full h-full items-center bg-gray-950 overflow-y-scroll">
+                <div className="flex flex-col items-center w-full lg:w-4xl h-full pt-6 gap-8 md:px-4">
                     <div className="flex flex-col gap-6 items-center">
 
-                        <div className="text-white text-6xl font-bold">
+                        <div className="text-white text-2xl md:text-4xl font-bold">
                             {"Cyber Defense Organization"}
                         </div>
-                        <img className="rounded hover:shadow-xl shadow-indigo-950 hover:scale-101 transform duration-300" src={hero} />
-
+                        <img className="md:rounded hover:shadow-xl shadow-indigo-950 hover:scale-101 transform duration-300 px-8 md:px-0" src={hero} />
                     </div>
 
-                    <div className="flex flex-col text-white w-full items-center gap-4">
-                        <div className="text-5xl text-white font-semibol">
+                    <div className="flex flex-col text-white w-full items-center gap-4 p-4 md:p-0 text-center md:text-left">
+                        <div className="text-2xl md:text-4xl text-white font-semibol">
                             {"Our Mission"}
                         </div>
 
-                        <div className="text-2xl text-stone-100">
+                        <div className="text-lg md:text-2xl text-stone-100">
                             {"Cyber Defense Organization is a student run organization dedicated towards teaching students practical skills in preparation for cyber security competitions and the real world. Through workshops, competitions, and teamwork, it is our mission to prepare students for careers in this rapidly evolving field. Whether you are completely new to cybersecurity or a seasoned professional we would love for you to join us in our club."}
                         </div>
                     </div>
 
-                    <div className="flex flex-row justify-between gap-4">
-                        <Card title="Interactive Workshops" body="Our organization holds weekly workshops during the semester to teach members new things in the security industry." />
-                        <Card title="Community" body="We've built a thriving discord community that allows members to discuss current security events, share resources, and network." />
-                        <Card title="Competition" body="We attend multiple competitions throughout the year focusing on red/blue/purple teaming & we even host our own competition, GDDC!" />
-                    </div>
-
-                    <div className="bg-white w-10 h-10">
+                    <div className="flex flex-col px-4 pb-4 md:flex-row md:px-0 justify-between gap-4">
+                        <Card title="Interactive Workshops" image={workshop} body="Our organization holds weekly workshops during the semester to teach members new things in the security industry." />
+                        <Card title="Community" image={community} body="We've built a thriving discord community that allows members to discuss current security events, share resources, and network." />
+                        <Card title="Competition" image={competition} body="We attend multiple competitions throughout the year focusing on red/blue/purple teaming & we even host our own competition, GDDC!" />
                     </div>
                 </div>
             </div>
