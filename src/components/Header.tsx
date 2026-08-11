125526ad17799ba3355a635bf35dde2a418667d8
coal-rock <colebrazinski15@gmail.com>
2026-08-11 01:26:19 -0400

    clean up header pt.2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,48 +1,36 @@
 import { Link, useLocation } from "react-router"
 import HamburgerMenu from "./HamburgerMenu.tsx"
-import logo from '../assets/logo.png'
+import Logo from "../assets/logo-white-est-text.png";
 import { routes } from "../data/navigationData.tsx";
 
 export default function Header() {
     const location = useLocation()
     return (
         <>
-            <header className='sticky top-0 z-50 bg-black w-full border-b-4 border-indigo-400'>
+            <header className='sticky top-0 z-50 bg-black w-full border-b border-[#e0e0e0]'>
 
-                <div className='mx-auto max-w-7xl px-4 h-16 flex items-center justify-between mb-4'>
+                <div className='w-full px-4 h-18 flex items-center justify-center'>
+                    <nav className='hidden md:flex items-between justify-between w-full'>
+                        <img src={Logo} className="h-12" />
 
-                    <div className='flex items-center gap-4'>
-                        {/* Logo as Home Button */}
-                        <Link to='/'
-                            onClick={() => {
-                                window.scrollTo({ top: 0, behavior: 'smooth' })
-                            }}
-                        >
-                            <img
-                                src={logo}
-                                alt='CDO Logo'
-                                className='h-32 mt-14'
-                            />
-                        </Link>
-                    </div>
+                        <div className="flex flex-row gap-8 justify-center items-center">
+                            {routes.map((route, index) => {
 
-                    <nav className='hidden md:flex items-center gap-8 mt-4'>
-                        {routes.map((route, index) => {
+                                const isActive = location.pathname === route.path
 
-                            const isActive = location.pathname === route.path
-
-                            return (
-                                <Link
-                                    key={index}
-                                    to={route.path}
-                                    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-                                    className={`text-lg md:text-xl font-medium tracking-wider uppercase transition-colors duration-200 hover:text-white
-                                        ${isActive ? 'text-indigo-400' : 'text-stone-300'}`}
-                                >
-                                    {route.name == 'menu' ? 'home' : route.name}
-                                </Link>
-                            )
-                        })}
+                                return (
+                                    <Link
+                                        key={index}
+                                        to={route.path}
+                                        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                                        className={`text-base font-bold tracking-wider uppercase hover:text-white text-[#e0e0e0]
+                                        ${isActive ? 'underline' : ''}`}
+                                    >
+                                        {route.name == 'menu' ? 'home' : route.name}
+                                    </Link>
+                                )
+                            })}
+                        </div>
                     </nav>
 
                     <div className='md:hidden block'>
